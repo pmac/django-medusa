7ad33014b4bb14de669a6209df33c290d9efc0f1
Brandon Taylor <brandon@iambrandontaylor.com>
2015-02-12 22:00:26 -0500

    Added MEDUSA_COLLECT_STATIC setting.

--- django_medusa/settings.py
+++ django_medusa/settings.py
@@ -2,5 +2,7 @@ from django.conf import settings
 
 
 MEDUSA_COLLECTSTATIC_IGNORE = getattr(settings,
-    'MEDUSA_COLLECTSTATIC_IGNORE', []
-)
+    'MEDUSA_COLLECTSTATIC_IGNORE', [])
+
+MEDUSA_COLLECT_STATIC = getattr(settings,
+    'MEDUSA_COLLECT_STATIC', False)
